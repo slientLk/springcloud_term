41376ec93b799c566ae69b9dec7d65247980cbe5
tg <852252567@qq.com>
2020-06-22 15:35:04 +0800

    hystrix dashboard

--- feign/src/main/java/com/feign/FeignApplication.java
+++ feign/src/main/java/com/feign/FeignApplication.java
@@ -2,10 +2,16 @@ package com.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class FeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeignApplication.class,args);
